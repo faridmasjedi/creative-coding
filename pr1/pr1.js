f4b5d62b2255470ba8e3654271010e290c7a5b95
farid118 <farid.masjedi1985@gmail.com>
2025-02-16 03:47:41 +1100

    Add new projects

---
+++ pr1/pr1.js
@@ -0,0 +1,44 @@
+const canvasSketch = require('canvas-sketch');
+
+const settings = {
+  dimensions: [ 2048, 2048 ]
+};
+
+const sketch = () => {
+  return ({ context, width, height }) => {
+    drawEachSecond(context, width, height)
+  };
+};
+
+const drawRects = (context) => {
+  const w = 150;
+  const h = 150;
+  
+  for(let i = w; i< Math.floor(2048/150)*150; i+=200){
+    for(let j= h; j <Math.floor(2048/150)*150; j+= 200){
+      context.beginPath();
+      context.rect(i, j, w,h);
+      context.stroke();
+
+      const randomNumber = w/4 * (Math.random());
+      context.beginPath();
+      context.lineWidth = Math.ceil(randomNumber);
+      context.rect(i+randomNumber, j+randomNumber, w-2*randomNumber, h-2*randomNumber)
+      context.stroke()
+    }
+  }
+}
+
+const resetCanvas = (context, width, height) => {
+  context.fillStyle = 'white';
+  context.fillRect(0, 0, width, height);
+}
+
+const drawEachSecond = (context, width, height) => {
+  setInterval(() => {
+    resetCanvas(context,width,height)
+    drawRects(context)
+  }, 1000)
+}
+
+canvasSketch(sketch, settings);
---
+++ pr3/pr3.js
@@ -0,0 +1,84 @@
+const canvasSketch = require('canvas-sketch');
+
+const settings = {
+  dimensions: [ 2048, 2048 ],
+  animate: true
+};
+
+const sketch = ({ context, width, height }) => {
+  const objInfo = { context, width, height }
+  const points = pointsArray(width, height, 40);
+  objInfo.points = points
+
+  return ({ context, width, height }) => {
+    context.fillStyle = 'white';
+    context.fillRect(0, 0, width, height);
+    pointsMotion(objInfo)
+  };
+};
+
+class Point {
+  constructor(x,y, r){
+    this.x = x
+    this.y = y
+    this.r = r
+    this.vX = Math.random()* 3
+    this.vY = Math.random()* 5
+  }
+
+  draw(ctx) {
+    ctx.lineWidth = '5'
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.r, 0, 2* Math.PI)
+    ctx.stroke()
+  }
+
+  move(width, height) { 
+    if (this.x <= 0 || this.x >= width) this.vX *= -1
+    if (this.y <= 0 || this.y >= height) this.vY *= -1
+
+    this.x += this.vX 
+    this.y += this.vY
+  }
+
+  drawLine(ctx, xx, yy){
+    const dx = this.x - xx
+    const dy = this.y - yy
+    const dist = Math.sqrt(dx **2 + dy ** 2)
+    if (dist > 200) return;
+    
+    ctx.lineWidth = '' + dist/100
+    ctx.beginPath()
+    ctx.moveTo(this.x, this.y)
+    ctx.lineTo(xx,yy)
+    ctx.fill()
+    ctx.stroke()
+  }
+
+}
+
+const pointsArray = (width, height, num) => {
+  const points = []
+  for (let i=0; i < num; i++){
+    const p = new Point(Math.random() * width, Math.random() * height, Math.random() * 19 + 3)
+    points.push(p)
+  }
+  return points;
+}
+
+const pointsMotion = (infoObj) => {
+  infoObj.points.forEach(point => {
+    point.draw(infoObj.context);
+    point.move(infoObj.width, infoObj.height);
+  });
+
+  for( let i = 0; i< infoObj.points.length; i++){
+    for(let j=i+1; j<infoObj.points.length; j++){
+      const currentP = infoObj.points[i];
+      const otherP = infoObj.points[j]
+      currentP.drawLine(infoObj.context,otherP.x, otherP.y)
+    }
+  }
+}
+
+canvasSketch(sketch, settings);
